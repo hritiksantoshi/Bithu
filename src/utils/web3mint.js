db1351ff9fc4544c27405ed4f6791cf4bb915f2b
hritiksantoshi <hritiksantoshi99@gmail.com>
2022-10-28 15:18:14 +0530

    layout

--- src/utils/web3mint.js
+++ src/utils/web3mint.js
@@ -8,7 +8,7 @@ export const mint = async (mint_amount) => {
     if(isMetaMaskInstalled()){
         const provider = new ethers.providers.Web3Provider(ethereum);
         const signer = provider.getSigner();
-        const contractAddress = "0x9FD56e423c9f0C01D4d011ad30860962ddaeA51D";
+        const contractAddress = "0xd8b934580fcE35a11B58C6D73aDeE468a2833fa8";
         const nftContract = new ethers.Contract(contractAddress, contract, signer);
         let txnHash = await nftContract.mint(ethereum.selectedAddress, mint_amount, {
             gasLimit: "285000",
@@ -24,7 +24,6 @@ export const totalMintCount = async () => {
         const signer = provider.getSigner();
         const contractAddress = "0x9FD56e423c9f0C01D4d011ad30860962ddaeA51D";
         const nftContract = new ethers.Contract(contractAddress, contract, signer);
-
         let totalMint = await nftContract.count();
 
         return totalMint;
