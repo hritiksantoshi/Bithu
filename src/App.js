5c5c06db717b9bbbdbf76f83046b41643e84a604
hritiksantoshi <hritiksantoshi99@gmail.com>
2022-10-28 10:38:04 +0530

    layout

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
-import React from "react";
-import GlobalStyles from "./component/styles/GlobalStyle";
-import { Header } from "./component/Header";
-import { Main } from "./component/Main";
-import CharacterSlider from "./component/characterSlider/CharacterSlider";
-import HowToMint from "./component/howToMint/HowToMint";
-import Team from "./component/team/Team";
-import {Faq} from "./component/Faq"
-import Footer from "./component/footer";
-const App = () => {
-  return <>
-  <GlobalStyles/>
-     <Header/> 
-     <Main />
-     <CharacterSlider/>
-     <HowToMint/>
-     <Team/>
-     <Faq/>
-     <Footer/>
-  </>
+import { Routes, Route } from "react-router-dom";
+import HomeV1 from "../src/pages/homeV1";
+
+function App() {
+  return (
+    <Routes>
+      <Route path="/" element={<HomeV1 />} exact />
+    </Routes>
+  );
 }
 
 export default App;
---
+++ src/pages/homeV1.jsx
@@ -0,0 +1,41 @@
+import { useModal } from "../utils/ModelContext";
+import GlobalStyles from "../component/styles/GlobalStyle";
+import Header from "../component/header/Header";
+import Layout from "../component/common/layout/Layout";
+import Banner from "../component/banner/Banner";
+import Counter from "../component/common/counter/Counter";
+import CharacterSlider from "../component/characterSlider/CharacterSlider";
+import HowToMint from "../component/howToMint/HowToMint";
+import About from "../component/about/About";
+import RoadMap from "../component/roadMap/RoadMap";
+import Team from "../component/team/Team";
+import {Faq} from "../component/Faq"; 
+import Footer from "../component/footer/Footer";
+// import MintNowModal from "../component/common/modal/mintNowModal/MintNowModal";
+import WalletModal from "../component/common/modal/walletModal/WalletModal";
+import MetamaskModal from "../component/common/modal/metamask/MetamaskModal";
+import ConnectWallet from "../component/common/modal/metamask/ConnectWallet";
+
+const HomeV1 = () => {
+  const {walletModalvisibility, metamaskModalVisibility, connectWalletModal } = useModal();
+  return (
+    <Layout>
+       <GlobalStyles />  
+      {walletModalvisibility && <WalletModal />}
+      {metamaskModalVisibility && <MetamaskModal/> }
+      {connectWalletModal && <ConnectWallet/> }
+      <Header />
+      <Banner />
+      <Counter />
+      <CharacterSlider />
+      <HowToMint />
+      <About />
+      <RoadMap /> 
+      <Team />
+      <Faq />
+      <Footer />
+    </Layout>
+  );
+};
+
+export default HomeV1;
